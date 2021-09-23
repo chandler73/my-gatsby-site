64a91d7be78e0942ebe655bb1704d580611dbea3
Chandler Molbert <chandlermolbert@Chandlers-MacBook-Pro.local>
2021-09-23 14:22:54 -0500

    Finished gatsby tutorial part 4.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => {
       <Layout pageTitle="Home Page">
           <p>I'm making this by following the Gatsby Tutorial.</p>
           <StaticImage
-              alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+              alt="Molbert family picture from a hike in Glacier National Park."
               src="../images/glacier_pic.jpg"
           />
       </Layout>
